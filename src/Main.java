c22752b42f18742ae509b5b97a5521a34fd56ac6
captain-dj <whardig10@gmail.com>
2023-09-24 14:37:12 -0400

    V1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner myObj = new Scanner(System.in);
+        String userName;
+
+        // Enter username and press Enter
+        System.out.println("Enter username");
+        userName = myObj.nextLine();
+
+        System.out.println("Username is: " + userName);
     }
 }
